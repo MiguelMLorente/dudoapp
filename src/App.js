c0bcde002c1712bd655dc10546fe14eb529998a4
Pau Costa <mico@micodev.es>
2021-11-08 13:21:21 +0100

    Initial setup  and early layout

--- src/App.js
+++ src/App.js
@@ -1,25 +1,30 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import styled from "styled-components";
+import { Hidden, Grid, Paper } from "@mui/material";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <React.Fragment key="App">
+      <StyledGrid container spacing={3}>
+        <Hidden only="xs">
+          <StyledGrid item sm={1} md={3}>
+            <Paper>GUTTER</Paper>
+          </StyledGrid>
+        </Hidden>
+        <StyledGrid item xs={12} sm={10} md={6}>
+          <Paper className="Content"> Here goes the content</Paper>
+        </StyledGrid>
+        <Hidden only="xs">
+          <StyledGrid item sm={1} md={3}>
+            <Paper>GUTTER</Paper>
+          </StyledGrid>
+        </Hidden>
+      </StyledGrid>
+    </React.Fragment>
   );
 }
 
+const StyledGrid = styled(Grid)`
+  text-align: center;
+`;
 export default App;
