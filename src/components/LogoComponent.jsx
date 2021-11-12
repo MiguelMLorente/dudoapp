63333386c0ffc4e9e29266cc35d38c23ab369d81
Pau Costa <mico@micodev.es>
2021-11-12 12:57:08 +0100

    Added small logo variant

--- src/components/LogoComponent.jsx
+++ src/components/LogoComponent.jsx
@@ -2,18 +2,34 @@ import styled from "styled-components";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faDice } from "@fortawesome/free-solid-svg-icons";
 
-function LogoComponent() {
-  return (
-    <StyledLogo className="logo">
-      <FontAwesomeIcon icon={faDice} />
-      DUDOAPP
-    </StyledLogo>
-  );
+function LogoComponent(props) {
+  switch (props.variant) {
+    case "small":
+      return (
+        <StyledSmallLogo className="logo">
+          <FontAwesomeIcon icon={faDice} />
+          DUDOAPP
+        </StyledSmallLogo>
+      );
+
+    default:
+      return (
+        <StyledLogo className="logo">
+          <FontAwesomeIcon icon={faDice} />
+          DUDOAPP
+        </StyledLogo>
+      );
+  }
 }
 
 const StyledLogo = styled.div`
   font-family: "Amatic SC", cursive;
   font-size: 5rem;
   padding: 2rem 5rem;
+`;
+const StyledSmallLogo = styled.div`
+  font-family: "Amatic SC", cursive;
+  font-size: 3rem;
+  padding: 2rem 2rem;
 `;
 export default LogoComponent;
