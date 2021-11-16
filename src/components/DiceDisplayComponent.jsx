2f4bf3aa1b6872a3a9d25c2b6d77a65154a25163
Pau Costa <mico@micodev.es>
2021-11-16 12:30:58 +0100

    Added DiceDisplayComponent

---
+++ src/components/DiceDisplayComponent.jsx
@@ -0,0 +1,46 @@
+import React from "react";
+import { Grid, Paper } from "@mui/material";
+import styled from "styled-components";
+import { v4 as uuid4 } from "uuid";
+import { valueToDiceIcon } from "../utils";
+
+function DiceDisplayComponent(props) {
+  let variantClassName = "";
+  switch (props.variant) {
+    default:
+      variantClassName = "big-dice";
+      break;
+    case "small":
+      variantClassName = "small-dice";
+      break;
+  }
+  return (
+    <React.Fragment>
+      <StyledPaper elevation={3}>
+        <div className={variantClassName}>
+          <Grid container justifyContent="center" spacing={3}>
+            {props.diceValues.map((value) => {
+              return (
+                <Grid item key={uuid4()}>
+                  {valueToDiceIcon(value)}
+                </Grid>
+              );
+            })}
+          </Grid>
+        </div>
+      </StyledPaper>
+    </React.Fragment>
+  );
+}
+
+const StyledPaper = styled(Paper)`
+  .big-dice {
+    padding: 2rem;
+    font-size: 2rem;
+  }
+  .small-dice {
+    /* TODO: ADD CSS FOR END OF ROUND DICE */
+  }
+`;
+
+export default DiceDisplayComponent;
---
+++ src/utils.js
@@ -0,0 +1,22 @@
+import {
+  faDiceOne,
+  faDiceTwo,
+  faDiceThree,
+  faDiceFour,
+  faDiceFive,
+  faDiceSix,
+} from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+
+export function valueToDiceIcon(value) {
+  const diceIconMap = {
+    1: faDiceOne,
+    2: faDiceTwo,
+    3: faDiceThree,
+    4: faDiceFour,
+    5: faDiceFive,
+    6: faDiceSix,
+  };
+
+  return <FontAwesomeIcon icon={diceIconMap[value]} />;
+}
