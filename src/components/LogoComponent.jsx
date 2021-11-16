ab56dc6b44772fe2751623b3652bd47e73ccd785
Pau Costa <mico@micodev.es>
2021-11-16 11:57:08 +0100

    Refactored variants of LogoComponent

--- src/components/LogoComponent.jsx
+++ src/components/LogoComponent.jsx
@@ -3,33 +3,37 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faDice } from "@fortawesome/free-solid-svg-icons";
 
 function LogoComponent(props) {
+  let variantClassName = "";
   switch (props.variant) {
-    case "small":
-      return (
-        <StyledSmallLogo className="logo">
-          <FontAwesomeIcon icon={faDice} />
-          DUDOAPP
-        </StyledSmallLogo>
-      );
-
     default:
-      return (
-        <StyledLogo className="logo">
-          <FontAwesomeIcon icon={faDice} />
-          DUDOAPP
-        </StyledLogo>
-      );
+      variantClassName = "big-logo";
+      break;
+    case "small":
+      variantClassName = "small-logo";
+      break;
   }
+
+  return (
+    <StyledLogo>
+      <div className={variantClassName}>
+        <FontAwesomeIcon icon={faDice} />
+        DUDOAPP
+      </div>
+    </StyledLogo>
+  );
 }
 
 const StyledLogo = styled.div`
-  font-family: "Amatic SC", cursive;
+  font-family: "Amatic SC";
   font-size: 5rem;
-  padding: 2rem 5rem;
+  .big-logo {
+    font-size: 5rem;
+    padding: 2rem 5rem;
+  }
+  .small-logo {
+    font-size: 3rem;
+    padding: 2rem 2rem;
+  }
 `;
-const StyledSmallLogo = styled.div`
-  font-family: "Amatic SC", cursive;
-  font-size: 3rem;
-  padding: 2rem 2rem;
-`;
+
 export default LogoComponent;
