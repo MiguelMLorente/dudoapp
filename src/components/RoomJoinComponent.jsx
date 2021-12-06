5ad80f7339e05cc811a5217631a6f7212bd352f3
Pau Costa <mico@micodev.es>
2021-12-06 12:08:09 +0100

    User has been kicked modal

--- src/components/RoomJoinComponent.jsx
+++ src/components/RoomJoinComponent.jsx
@@ -1,17 +1,26 @@
 import React, { useContext, useEffect, useState } from "react";
 import styled from "styled-components";
-import { TextField, Grid, Button, Paper } from "@mui/material";
+import {
+  TextField,
+  Grid,
+  Button,
+  Paper,
+  Modal,
+  Box,
+  Typography,
+} from "@mui/material";
 import LogoComponent from "./LogoComponent";
 import { useDispatch, useSelector } from "react-redux";
 import { WebSocketContext } from "../WebSocket";
 import { updateUserName } from "../actions/gameDataActions";
-import { changeToNewRoom } from "../actions/appStatusActions";
+import { changeToNewRoom, updateKickStatus } from "../actions/appStatusActions";
 
 function RoomJoinComponent() {
   const ws = useContext(WebSocketContext);
   const userUuid = useSelector((state) => state.gameData.userId);
   const dispatch = useDispatch();
   let error = useSelector((state) => state.gameData.error);
+  let userKicked = useSelector((state) => state.appStatus.userKicked);
   let [userName, setUserName] = useState(
     useSelector((state) => state.gameData.name)
   );
@@ -23,6 +32,18 @@ function RoomJoinComponent() {
   let [roomIdHelperText, setRoomIdHelperText] = useState("");
   let [triedToJoin, setTriedToJoin] = useState(false);
   let [gameNotFound, setGameNotFound] = useState(false);
+
+  const modalBoxStyle = {
+    position: "absolute",
+    top: "50%",
+    left: "50%",
+    transform: "translate(-50%, -50%)",
+    width: 400,
+    bgcolor: "background.paper",
+    border: "2px solid #000",
+    boxShadow: 24,
+    p: 4,
+  };
 
   const userNameHandler = (e) => {
     setUserName(e.target.value);
@@ -73,6 +94,10 @@ function RoomJoinComponent() {
     dispatch(changeToNewRoom());
   };
 
+  const handleClose = () => {
+    dispatch(updateKickStatus(false));
+  };
+
   useEffect(() => {
     if (error === "Game not found") {
       setRoomIdHelperText("Game does not exist");
@@ -87,6 +112,17 @@ function RoomJoinComponent() {
   return (
     <React.Fragment>
       <StyledContainer>
+        <Modal open={userKicked} onClose={handleClose}>
+          <Box sx={modalBoxStyle}>
+            <Typography id="modal-modal-title" variant="h6" component="h2">
+              You have been kicked
+            </Typography>
+            <Typography id="modal-modal-description" sx={{ mt: 2 }}>
+              Sorry, you have been kicked by the admin of the room, click
+              outside this box to continue
+            </Typography>
+          </Box>
+        </Modal>
         <StyledGrid container>
           <Grid item s={12} sm={10} md={8} lg={6}>
             <StyledPaper>
