11a1d27ecc7a0a795bf42a295e5920c295c03bb5
Pau Costa <mico@micodev.es>
2022-01-11 12:33:55 +0100

    initial layout of round recap

--- src/components/InGameComponents/EndOfRoundComponent.jsx
+++ src/components/InGameComponents/EndOfRoundComponent.jsx
@@ -1,18 +1,23 @@
 import React from "react";
-import { Grid, Paper } from "@mui/material";
+import { Grid, Paper, Typography } from "@mui/material";
 import styled from "styled-components";
 import { useSelector } from "react-redux";
 import DiceDisplayComponent from "./DiceDisplayComponent";
+import { red } from "@mui/material/colors";
 
 function EndOfRoundComponent() {
   let playersInfo = useSelector((state) => state.gameStatus.playersInfo);
+  let requesterName = useSelector((state) => state.gameStatus.requesterName);
+  let winnerName = useSelector((state) => state.gameStatus.winnerName);
+  let loserName = useSelector((state) => state.gameStatus.loserName);
+  let action = useSelector((state) => state.gameStatus.action);
 
   return (
     <React.Fragment>
       <StyledContainer>
         <StyledGrid container>
-          <Grid item s={12} sm={10} md={8} lg={6}>
-            <StyledPaper>
+          <Grid item xs={12} sm={10} md={8}>
+            <StyledPaper elevation={1}>
               <Grid
                 container
                 direction="column"
@@ -20,10 +25,34 @@ function EndOfRoundComponent() {
                 spacing={5}
                 className="EndOfRoundContainer"
               >
+                {" "}
+                <Grid item>
+                  <Typography>{requesterName}: </Typography>
+                </Grid>
+                <Grid item>
+                  <StyledPaper elevation={2}>
+                    <Grid container item>
+                      <Grid container item direction="column" xs={6}>
+                        <Grid item>{action}</Grid>
+                        <Grid container item justifyContent="center">
+                          <Grid item>val</Grid>
+                          <Grid item>num</Grid>
+                        </Grid>
+                      </Grid>
+                      <Grid container item direction="column" xs={6}>
+                        <Grid item>Actual</Grid>
+                        <Grid container item justifyContent="center">
+                          <Grid item>val</Grid>
+                          <Grid item>num</Grid>
+                        </Grid>
+                      </Grid>
+                    </Grid>
+                  </StyledPaper>
+                </Grid>
                 {playersInfo.map((player) => {
                   return (
-                    <Grid item key={player.playerName}>
-                      <StyledDiceDisplayComponent
+                    <Grid item key={player.playerName} s={12}>
+                      <DiceDisplayComponent
                         playerName={player.playerName}
                         diceValues={player.diceValue}
                         variant="small"
@@ -39,8 +68,6 @@ function EndOfRoundComponent() {
     </React.Fragment>
   );
 }
-
-const StyledDiceDisplayComponent = styled(DiceDisplayComponent)``;
 
 const StyledPaper = styled(Paper)`
   padding: 2rem;
